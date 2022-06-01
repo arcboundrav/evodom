acba79cbfffa31869d8fca6a7ed9e473e984fe21
arcboundrav <wotmudraiste@gmail.com>
2022-06-01 19:09:09 -0400

    Random sessions now use _ANOTHER_KINGDOM for debug purposes.

--- src/evolvedominion/play.py
+++ src/evolvedominion/play.py
@@ -7,9 +7,10 @@ from evolvedominion.agents.strategy import (
     EchoStrategy,
     EchoRandomStrategy,
 )
+from evolvedominion.engine.session import _ANOTHER_KINGDOM
 from evolvedominion.display.echo import EchoSession
 from evolvedominion.display.text import newline, summarize_session
-
+from evolvedo
 
 def _load_winning_phenotypes(simname):
     simulation_data = DATA_MANAGER.load(simname)
@@ -40,7 +41,7 @@ def _create_session(winning_phenotypes):
 def _get_random_opponent_session():
     players = [EchoPlayer(pid=0)]
     players.extend(EchoRandomStrategy(pid=i) for i in range(1, 4))
-    session = EchoSession()
+    session = EchoSession(kingdom=_ANOTHER_KINGDOM)
     session.accept_players(players)
     return session
 
